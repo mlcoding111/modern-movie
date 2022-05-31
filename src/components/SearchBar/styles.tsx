d11abd22fd6b9c5606fd6bc09b8b4dd65b2f7368
mlcoding111 <kennycodes@hotmail.com>
2022-05-30 23:07:55 -0400

    Fixed SearchBar width to be too large on mobile

--- src/components/SearchBar/styles.tsx
+++ src/components/SearchBar/styles.tsx
@@ -3,17 +3,24 @@ import styled from 'styled-components'
 export const SearchInput = styled.input`
     background-color: transparent;
     border: 2px solid var(--primary-color);
-    padding: 0.5rem .2rem;
+    padding: 0.5rem 1rem;
     border-radius: 50px;
     font-size: 1rem;
     color: #fff;
+    box-sizing: border-box;
+
     font-family: inherit;
     ::placeholder{
-    color: #7378c5;
+        color: #7378c5;
     }
     
     :focus{
     outline: 0;
     background-color: var(--primary-color);
-}
+    }
+    
+    @media screen and (max-width: 300px){
+        padding: .2rem 1rem;
+        width: 150px;
+    }
 `
